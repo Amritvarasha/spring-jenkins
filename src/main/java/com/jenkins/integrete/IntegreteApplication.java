257c12d55be08173a10d5662d81350967998ffb6
Varasha <avarasha@deloitte.com>
2022-12-05 12:42:54 +0530

    first commit

---
+++ src/main/java/com/jenkins/integrete/IntegreteApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.integrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegreteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkins/integrete/IntegreteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.integrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegreteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
